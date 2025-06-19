13915ce05e073a90a32e58dfb7a3a0e9121705a1
poorna <poornasurapuraju28@gmail.com>
2025-06-19 17:42:27 +0530

    changes

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,4 +1,3 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
 import '../../App.css';
 import { useNavigate } from 'react-router-dom';
 
@@ -6,19 +5,17 @@ const Header = () => {
   const navigate = useNavigate();
 
   const handleLogout = () => {
-    // ✅ Clear auth data
     localStorage.removeItem('authToken');
     localStorage.removeItem('user');
 
-    // ✅ Redirect to login
     navigate('/');
   };
 
   return (
-    <header className="header-fixed d-flex justify-content-between align-items-center px-4">
+    <header className="header-fixed d-flex justify-content-end align-items-center px-4">
       <div className="d-flex align-items-center gap-2">
         <i className="fas fa-search text-muted"></i>
-        <input type="text" className="form-control form-control-sm" placeholder="Search..." />
+        <input type="text" className="form-control form-control-sm me-3	" placeholder="Search..." />
       </div>
       <div className="d-flex align-items-center gap-3">
         <i className="fas fa-bell text-secondary"></i>
